09de3b3b9d33d5b5e7daa7361db416bd6914e342
Jessica <jessicauche93@gmail.com>
2022-06-24 09:49:38 +0100

    introducing myself to javascript

---
+++ Intro_to_javascript.js
@@ -0,0 +1,16 @@
+
+const firstName = "Jessica";
+const lastName = "Nwosu";
+const middleName = "Uche";
+
+const fullName = firstName + " " + middleName + " " + lastName;
+
+console.log(fullName)
+
+const height = "5.8";
+
+console.log(height)
+
+const country = "Nigeria";
+
+console.log(country)
